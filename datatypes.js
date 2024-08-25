ecb7c3d0f62b1c5c5a6b0ada94e5f6d80e320c6b
Tirthrajsinh007 <tirthrajsinhparmar007@gmail.com>
2024-08-25 19:08:41 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,48 @@
+//Data Types
+
+//numbers
+var myFavnum = 19 ;
+console.log(myFavnum);
+console.log(typeof myFavnum);
+
+
+// string 
+var my_Name ='tirthrajsinh';
+console.log(my_Name);
+console.log(typeof my_Name);
+
+
+//Boolean
+var isRaining =false;
+var AreyouFine =true;
+console.log(isRaining);
+console.log(typeof isRaining);
+
+//Undefined
+var dog;
+console.log(dog);
+
+
+//Null
+var head =null;
+console.log(head);
+
+
+//bigint
+var big=5459545379666533n;
+console.log(big);
+console.log(typeof big);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
